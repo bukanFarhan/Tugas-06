0c81af9baf8c73035db9ffea8ead32a411032448
muhammad farhan <muhammad.2842@students.amikom.ac.id>
2023-07-15 18:01:43 +0700

    Tugas 06

---
+++ 19.11.2842_Tugas06/Orang.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _19._11._2842_Tugas06
+{
+   internal class Orang
+    {
+		// Field public
+		public string Nama;
+
+		// Field private
+		private int Umur;
+
+		// Field protected
+		protected string Alamat;
+
+		// Construktor
+		public Orang(string nama, int umur, string alamat)
+		{
+			this.Nama = nama;
+			this.Umur = umur;
+			this.Alamat = alamat;
+		}
+
+		// Method 
+		public void TampilanInfo()
+		{
+
+			Console.WriteLine("=== Informasi Human ===\n");
+			Console.WriteLine("Nama   : {0}", this.Nama);
+			Console.WriteLine("Umur   : {0}", this.Umur);
+			Console.WriteLine("Alamat : {0}", this.Alamat);
+			Console.WriteLine("=====================================");
+		}
+
+		public void UpdateInfo()
+		{
+			Console.WriteLine("\n=== Update Informasi Human ===\n");
+			Console.WriteLine("Update Nama   : {0}", this.Nama);
+			Console.WriteLine("Update Umur   : {0}", this.Umur);
+			Console.WriteLine("Update Alamat : {0}", this.Alamat);
+			Console.WriteLine("===========================================");
+		}
+	}
+}
---
+++ 19.11.2842_Tugas06/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _19._11._2842_Tugas06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Orang orang1 = new Orang("Muhammad Farhan Anggara", 28, "Jl.Sawo No.197");
+            orang1.TampilanInfo();
+
+            Orang orang2 = new Orang("Eky Sumaikik", 15, "Jl.SanaSini No.80");
+            orang2.UpdateInfo();
+
+            Console.ReadKey();
+        }
+    }
+}
